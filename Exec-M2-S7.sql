5d64b64bfbddf578adf74560af9db4f0ce616648
Erico Couto Jr <erico.coutjr@gmail.com>
2023-06-18 11:58:13 -0300

    feat: Exercicios 1 e 2

---
+++ Exec-M2-S7.sql
@@ -0,0 +1,26 @@
+create table usuarios (
+	id int primary key,
+	nome varchar(40),
+	cpf varchar(11),
+	rg varchar(20),
+	data_nascimento date (DATEDIFF(YEAR, data_nascimento, GETDATE()) >= 18),
+	login varchar(100),
+	senha varchar(50)
+);
+
+create table jogos (
+	id int primary key,
+	nome_jogo varchar(255),
+	data_lancamento date,
+	genero varchar(255),
+	plataformas varchar(255),
+	path_video_foto varchar(255)
+);
+
+create table biblioteca (
+	id int primary key,
+	id_jogo int,
+	id_usuario int,
+	foreign key (id_jogo) references jogos(id),
+	foreign key (id_usuario) references usuarios(id)
+);
